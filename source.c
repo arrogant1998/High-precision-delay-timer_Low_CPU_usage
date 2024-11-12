4581974764452d234f200a699a48db8f06cc12a1
arrogant1998 <84486909+arrogant1998@users.noreply.github.com>
2024-11-12 16:00:42 +0800

    Update source.c

--- source.c
+++ source.c
@@ -4,9 +4,16 @@ void DelayMilliseconds(double milliseconds) {
     LARGE_INTEGER frequency, start, end;
 
     QueryPerformanceFrequency(&frequency);
+    double _waitCounts = ((milliseconds - 50) / 1e3) * frequency.QuadPart;
     double waitCounts = (milliseconds / 1e3) * frequency.QuadPart;
+
     QueryPerformanceCounter(&start);
-    Sleep(((int)milliseconds - (int)milliseconds % 50));
+    do {
+
+        Sleep(1);
+        QueryPerformanceCounter(&end);
+
+    } while ((end.QuadPart - start.QuadPart) < _waitCounts);
     do {
 
         QueryPerformanceCounter(&end);
